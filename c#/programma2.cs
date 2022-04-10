a579af9d53f06fd6315b62fbe01e619c3eac38c3
Ilja526 <94162285+Ilja526@users.noreply.github.com>
2022-04-10 19:39:47 +0300

    Update programma2.cs

--- c#/programma2.cs
+++ c#/programma2.cs
@@ -3,3 +3,33 @@ using System.Collections.Generic;
 using System.Text;
 using System.Xml;
 using System.IO;
+namespace xmlfails{
+    class Programms2{
+        public static void Main(string[] args){
+            XmlDocument xDocu = new XmlDocument();
+            xDocu.Load("text.xml");
+            XmlElement? xRoot = xDoc.DocumentElement;
+
+            XmlElement informElem = xDoc.CreateElement("information");
+ 
+            XmlElement nameElem = xDoc.CreateElement("name");
+            XmlElement surnameElem = xDoc.CreateElement("surname");
+            XmlElement emailElem = xDoc.CreateElement("email");
+ 
+            XmlText nameText = xDoc.CreateTextNode("Edgars");
+            XmlText surnameText = xDoc.CreateTextNode("Jansons");
+            XmlText emailText = xDoc.CreateTextNode("Jansons8797@gmail.com");
+ 
+            nameAttr.AppendChild(nameText);
+            surnameElem.AppendChild(surnameText);
+            emailElem.AppendChild(emailText);
+ 
+            informElem.Attributes.Append(nameAttr);
+            informElem.AppendChild(surnameElem);
+            informElem.AppendChild(emailElem);
+            xRoot?.AppendChild(informElem);
+
+            xDocu.Save("text.xml");
+        }
+    }
+}
