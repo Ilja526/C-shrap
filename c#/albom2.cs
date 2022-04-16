f995f597e935cee6a8f4cf61a09c8411eea29fbb
Ilja526 <94162285+Ilja526@users.noreply.github.com>
2022-04-16 15:59:03 +0300

    Update and rename programma2.cs to albom2.cs

---
+++ c#/albom2.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+namespace xmlfails{
+    class Programms2{
+        protected:
+            XmlDocument xmlDocu;
+        protected:
+            XmlElement informatElem;
+            XmlElement nameElem;
+            XmlElement albumElem;    
+    }
+    class Programm3:protected Programms2{
+        public:
+            void alboms(){
+                XmlDocument xmlDocu = new XmlDocument();
+                xmlDocu.Load("albom.xml");
+                XmlElement? xRoot = xDoc.DocumentElement;
+
+                XmlElement informatElem = xmlDocu.CreateElement("information");
+
+                XmlElement nameElem = xDoc.CreateElement("name");
+                XmlElement album_artistElem = xDoc.CreateElement("albom");
+
+                XmlText nameText = xDoc.CreateTextNode("Ilja");
+                XmlText album_artistText = xDoc.CreateTextNode("HARDIN");
+
+                nameElem.AppendChild(nameText);
+                album_artistElem.AppendChild(albumText);
+
+                informatElem.AppendChild(nameElem);
+                informatElem.AppendChild(album_artistElem);
+
+                xRoot?.AppendChild(informatElem);
+
+                xmlDocu.Save("albom.xml");
+            }
+    }
+}
