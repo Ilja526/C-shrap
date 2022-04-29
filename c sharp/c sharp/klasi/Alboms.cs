bfb258f266fd51b12133d4f370fe7b4f9ce15c33
Ilja526 <94162285+Ilja526@users.noreply.github.com>
2022-04-29 15:12:13 +0300

    Update Alboms.cs

--- c sharp/c sharp/klasi/Alboms.cs
+++ c sharp/c sharp/klasi/Alboms.cs
@@ -10,7 +10,7 @@ namespace c_sharp
         protected XmlDocument xmlDocu;
         protected XmlElement informatElem;
         protected XmlElement nameElem;
-        public static void AddAlbom(string albomName,string s)
+        public static void AddAlbom(string objetext, string idtext, string nametext, string album_artisttext, string yeartext, string zanrstext)
         {
             XmlDocument xmlDocu = new XmlDocument();
             xmlDocu.Load("../../../Data/alboms.xml");
@@ -18,19 +18,35 @@ namespace c_sharp
 
             XmlElement informatElem = xmlDocu.CreateElement("information");
 
+            XmlElement objeElem = xmlDocu.CreateElement("objekts");
+            XmlElement idElem = xmlDocu.CreateElement("id");
             XmlElement nameElem = xmlDocu.CreateElement("name");
-            XmlElement album_artistElem = xmlDocu.CreateElement("albom");
+            XmlElement album_artistElem = xmlDocu.CreateElement("album_artists");
+            XmlElement yearElem = xmlDocu.CreateElement("year");
+            XmlElement zanrsElem = xmlDocu.CreateElement("zanrs");
 
-            XmlText nameText = xmlDocu.CreateTextNode(albomName);
-            XmlText album_artistText = xmlDocu.CreateTextNode(s);
+            XmlText objeText = xmlDocu.CreateTextNode(objetext);
+            XmlText idText = xmlDocu.CreateTextNode(idtext);
+            XmlText nameText = xmlDocu.CreateTextNode(nametext);
+            XmlText album_artistText = xmlDocu.CreateTextNode(album_artisttext);
+            XmlText yearText = xmlDocu.CreateTextNode(yeartext);
+            XmlText zanrsText = xmlDocu.CreateTextNode(zanrstext);
 
+            objeElem.AppendChild(objeText);
+            idElem.AppendChild(idText);
             nameElem.AppendChild(nameText);
             album_artistElem.AppendChild(album_artistText);
-
+            yearElem.AppendChild(album_artistText);
+            zanrsElem.AppendChild(zanrsText);
+            
+            informatElem.AppendChild(objeElem);
+            informatElem.AppendChild(idElem);
             informatElem.AppendChild(nameElem);
             informatElem.AppendChild(album_artistElem);
+            informatElem.AppendChild(yearElem);
+            informatElem.AppendChild(zanrsElem);
 
-            xRoot?.AppendChild(informatElem);
+            xRoot.AppendChild(informatElem);
 
             xmlDocu.Save("../../../Data/alboms.xml");
         }
