ee253314fb2cc5ec3bed3f600498ff86518025e4
Ilja526 <94162285+Ilja526@users.noreply.github.com>
2022-04-30 15:11:05 +0300

    Add files via upload

---
+++ c sharp/c sharp/xml-read-music2.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.IO;
+using System.Xml.Linq;
+namespace c_sharp
+{
+    class Programm15{
+        protected XDocument xdoc;
+        protected XElement music;
+    }
+    class Programm16 : Programm15{
+            public void XMLread_music2(){
+                XDocument xdoc = XDocument.Load("../../../Data/music2.xml");
+                XElement music = xdoc.Element("music");
+                if(music != null){
+                    foreach (XElement information in music.Elements("information")){
+                        XElement objekts = information.Element("objekts");
+                        XElement id = information.Element("id");
+                        XElement name = information.Element("name");
+                        XElement surname = information.Element("surname");
+                        XElement status = information.Element("status");
+                        XElement email = information.Element("email");
+ 
+                        Console.WriteLine($"Objekts: {objekts.Value}");
+                        Console.WriteLine($"Id: {id.Value}");
+                        Console.WriteLine($"Name: {name.Value}");
+                        Console.WriteLine($"Surname: {surname.Value}");
+                        Console.WriteLine($"Status: {status.Value}");
+                        Console.WriteLine($"Email: {email.Value}");
+ 
+                        Console.WriteLine();
+                    }
+                }
+            }
+    } 
+}
