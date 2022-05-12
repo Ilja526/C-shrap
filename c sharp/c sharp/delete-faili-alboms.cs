72ae9b9ca73aaa4e629a24eafdfbd9ce28856698
Ilja526 <94162285+Ilja526@users.noreply.github.com>
2022-05-12 20:12:18 +0300

    Update delete-faili-alboms.cs

--- c sharp/c sharp/delete-faili-alboms.cs
+++ c sharp/c sharp/delete-faili-alboms.cs
@@ -14,28 +14,8 @@ namespace c_sharp{
             xDoc.Load("../../../Data/alboms.xml");
             XmlElement xRoot = xDoc.DocumentElement;
             XmlNode firstNode = xRoot.FirstChild;
-            XmlNode secondNode = xRoot.SecondChild;
-            XmlNode thirdNode = xRoot.ThirdChild;
-            XmlNode fourthNode = xRoot.FourthChild;
-            XmlNode fifthNode = xRoot.FifthChild;
-            XmlNode sixthNode = xRoot.SixthChild;
             if(firstNode != null){
                 xRoot.RemoveChild(firstNode);
-            }
-            if(secondNode != null){
-                xRoot.RemoveChild(secondNode);
-            }  
-            if(thirdNode != null){
-                xRoot.RemoveChild(thirdNode);
-            }  
-            if(fourthNode != null){
-                xRoot.RemoveChild(fourthNode);
-            }
-            if(fifthNode != null){
-                xRoot.RemoveChild(fifthNode);
-            }  
-            if(sixthNode != null){
-                xRoot.RemoveChild(sixthNode);
             }  
             xDoc.Save("../../../Data/alboms.xml");
         }
