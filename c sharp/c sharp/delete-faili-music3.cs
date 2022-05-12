867e308573e667498aa1b8933eac9aa603e72517
Ilja526 <94162285+Ilja526@users.noreply.github.com>
2022-05-12 20:13:42 +0300

    Update delete-faili-music3.cs

--- c sharp/c sharp/delete-faili-music3.cs
+++ c sharp/c sharp/delete-faili-music3.cs
@@ -14,29 +14,9 @@ namespace c_sharp{
             xDoc.Load("../../../Data/music3.xml");
             XmlElement xRoot = xDoc.DocumentElement;
             XmlNode firstNode = xRoot.FirstChild;
-            XmlNode secondNode = xRoot.SecondChild;
-            XmlNode thirdNode = xRoot.ThirdChild;
-            XmlNode fourthNode = xRoot.FourthChild;
-            XmlNode fifthNode = xRoot.FifthChild;
-            XmlNode sixthNode = xRoot.SixthChild;
             if(firstNode != null){
                 xRoot.RemoveChild(firstNode);
             }
-            if(secondNode != null){
-                xRoot.RemoveChild(secondNode);
-            }  
-            if(thirdNode != null){
-                xRoot.RemoveChild(thirdNode);
-            }  
-            if(fourthNode != null){
-                xRoot.RemoveChild(fourthNode);
-            }
-            if(fifthNode != null){
-                xRoot.RemoveChild(fifthNode);
-            }  
-            if(sixthNode != null){
-                xRoot.RemoveChild(sixthNode);
-            }  
             xDoc.Save("../../../Data/music3.xml");
         }
     }
