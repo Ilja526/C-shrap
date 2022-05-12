4b06da44e197a523e320ffa9fc94c7fb8daff68e
Ilja526 <94162285+Ilja526@users.noreply.github.com>
2022-05-12 19:03:34 +0300

    Update delete-faili-music3.cs

--- c sharp/c sharp/delete-faili-music3.cs
+++ c sharp/c sharp/delete-faili-music3.cs
@@ -4,12 +4,12 @@ using System.Text;
 using System.Xml;
 using System.IO;
 namespace c_sharp{
-    class Programm40{
+    class Programm71{
         protected XmlDocument xDoc;
         protected XmlElement xRoot;
         protected XmlNode firstNode;
 
-        public void delete_faile_music3(){
+        public static void delete_faile_music3(){
             XmlDocument xDoc = new XmlDocument();
             xDoc.Load("../../../Data/music3.xml");
             XmlElement xRoot = xDoc.DocumentElement;
