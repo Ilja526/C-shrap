7a99331a77e55a0a33aeb6e9f785cccab0de2bd5
Ilja526 <94162285+Ilja526@users.noreply.github.com>
2022-05-12 19:01:32 +0300

    Update delete-faili-music.cs

--- c sharp/c sharp/delete-faili-music.cs
+++ c sharp/c sharp/delete-faili-music.cs
@@ -4,13 +4,13 @@ using System.Text;
 using System.Xml;
 using System.IO;
 namespace c_sharp{
-    class Programm40{
+    class Programm60{
         //Šos mainīgos aizsargā protected funkcija, tāpēc tiem var piekļūt, tikai izmantojot metodi.
         protected XmlDocument xDoc;
         protected XmlElement xRoot;
         protected XmlNode firstNode;
 
-        public void delete_faile_music(){
+        public static void delete_faile_music(){
             XmlDocument xDoc = new XmlDocument();
             xDoc.Load("../../../Data/music.xml");//Šeit mēs norādām ceļu uz failu, ierakstot tā nosaukumu.
             XmlElement xRoot = xDoc.DocumentElement;
