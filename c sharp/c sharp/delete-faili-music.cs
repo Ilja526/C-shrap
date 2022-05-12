4080544ea2dde0d8b8ab50aa3d2c1b7bd196ccb2
Ilja526 <94162285+Ilja526@users.noreply.github.com>
2022-05-12 20:13:01 +0300

    Update delete-faili-music.cs

--- c sharp/c sharp/delete-faili-music.cs
+++ c sharp/c sharp/delete-faili-music.cs
@@ -15,29 +15,9 @@ namespace c_sharp{
             xDoc.Load("../../../Data/music.xml");//Šeit mēs norādām ceļu uz failu, ierakstot tā nosaukumu.
             XmlElement xRoot = xDoc.DocumentElement;
             XmlNode firstNode = xRoot.FirstChild;
-            XmlNode secondNode = xRoot.SecondChild;
-            XmlNode thirdNode = xRoot.ThirdChild;
-            XmlNode fourthNode = xRoot.FourthChild;
-            XmlNode fifthNode = xRoot.FifthChild;
-            XmlNode sixthNode = xRoot.SixthChild;
             if(firstNode != null){ //Šeit, izmantojot operatoru if, mēs pārbaudām, vai failā ir mezgli vai nav.
                 xRoot.RemoveChild(firstNode);
             }
-            if(secondNode != null){
-                xRoot.RemoveChild(secondNode);
-            }  
-            if(thirdNode != null){
-                xRoot.RemoveChild(thirdNode);
-            }  
-            if(fourthNode != null){
-                xRoot.RemoveChild(fourthNode);
-            }
-            if(fifthNode != null){
-                xRoot.RemoveChild(fifthNode);
-            }  
-            if(sixthNode != null){
-                xRoot.RemoveChild(sixthNode);
-            }  
             xDoc.Save("../../../Data/music.xml");
         }
     }
