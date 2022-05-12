ecb6143052d7fdb077205db6f788bb6120bf43e5
Ilja526 <94162285+Ilja526@users.noreply.github.com>
2022-05-12 19:04:04 +0300

    Update delete-faili-songs.cs

--- c sharp/c sharp/delete-faili-songs.cs
+++ c sharp/c sharp/delete-faili-songs.cs
@@ -4,12 +4,12 @@ using System.Text;
 using System.Xml;
 using System.IO;
 namespace c_sharp{
-    class Programm40{
+    class Programm72{
         protected XmlDocument xDoc;
         protected XmlElement xRoot;
         protected XmlNode firstNode;
 
-        public void delete_faile_songs(){
+        public static void delete_faile_songs(){
             XmlDocument xDoc = new XmlDocument();
             xDoc.Load("../../../Data/songs.xml");
             XmlElement xRoot = xDoc.DocumentElement;
